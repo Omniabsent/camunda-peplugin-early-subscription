d18c6fee1485bd3f121e9abe6d41969355bd6a6d
Dennis Wolf <dennis.wolf0815@gmail.com>
2017-09-07 11:41:39 +0200

    DEMO: added eurotunnel and flightbooking examples

--- flexsub-camunda-demoprocesses/src/test/java/hpi/bpt/flexsub/camunda/demoprojects/InMemoryH2Test.java
+++ flexsub-camunda-demoprocesses/src/test/java/hpi/bpt/flexsub/camunda/demoprojects/InMemoryH2Test.java
@@ -1,11 +1,8 @@
 package hpi.bpt.flexsub.camunda.demoprojects;
 
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.init;
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.processEngine;
 
 import org.apache.ibatis.logging.LogFactory;
-import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
@@ -36,22 +33,11 @@ public class InMemoryH2Test {
 	 * Just tests if the process definition is deployable.
 	 */
 	@Test
-	@Deployment(resources = "subscribe-on-deployment.bpmn")
+	@Deployment(resources = { "subscribe-on-deployment.bpmn", "subscribe-on-instantiation.bpmn",
+			"flight-booking-consume-buffer.bpmn", "eurotunnel.bpmn" })
 	public void testParsingAndDeployment() {
 		// nothing is done here, as we just want to check for exceptions during
 		// deployment
-	}
-
-	@Test
-	@Deployment(resources = "subscribe-on-deployment.bpmn")
-	public void testHappyPath() {
-
-		ProcessInstance processInstance = processEngine().getRuntimeService()
-				.startProcessInstanceByKey("flexsub.camunda.demoprojects.simple.SubscribeOnDeployment");
-
-		processEngine().getRuntimeService().correlateMessage("Message_2nff1at");
-		assertThat(processInstance).isEnded();
-
 	}
 
 }
